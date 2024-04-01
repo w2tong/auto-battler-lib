b9494b523bd5aaec1ddf74b93b8db54bb159c1fd
Wayne Tong <dkotrzarashi@gmail.com>
2024-04-01 19:47:56 -0400

    add damageRoll function to DamageRange

--- src/DamageRange.ts
+++ src/DamageRange.ts
@@ -4,4 +4,9 @@ interface DamageRange {
     bonus: number;
 }
 
+function damageRoll({min, max, bonus}: DamageRange): number {
+    return min + Math.floor(Math.random() * (max - min + 1)) + bonus;
+}
+
 export default DamageRange;
+export { damageRoll };
