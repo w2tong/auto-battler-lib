f5895a128f63abf147809840dc04695b6a212010
Wayne Tong <dkotrzarashi@gmail.com>
2024-05-08 03:42:05 -0400

    update Firebolt tdamage to scale with level instead of weapons

--- src/Ability/Firebolt.ts
+++ src/Ability/Firebolt.ts
@@ -3,6 +3,10 @@ import DebuffId from '../StatusEffect/DebuffId';
 import Ability from './Ability';
 
 const NAME = 'Firebolt';
+const MIN_BASE = 1;
+const MIN_PER_LVL = 0.1;
+const MAX_BASE = 4;
+const MAX_PER_LVL = 0.2;
 const SPELLPOWER_RATIO = 0.34;
 
 const Firebolt: Ability = {
@@ -16,7 +20,11 @@ const Firebolt: Ability = {
             const hit = char.attack({
                 target: char.target,
                 attackType: AttackType.Spell,
-                damageRange: char.mainHand.damageRange,
+                damageRange: {
+                    min: MIN_BASE + char.level * MIN_PER_LVL,
+                    max: MAX_BASE + char.level * MAX_PER_LVL,
+                    bonus: 0
+                },
                 spellPowerRatio: SPELLPOWER_RATIO,
                 isOffHand: false,
                 abilityName: NAME
