855ad740053c658129e1a62b8b2b1676b86bd1ce
Wayne Tong <w3tong@gmail.com>
2025-03-03 14:05:50 -0500

    add test-battle character Damage Range stat

--- src/test/battle.ts
+++ src/test/battle.ts
@@ -1,7 +1,8 @@
+import AttackType from '../AttackType';
 import Battle from '../Battle';
 import Character from '../Character/Character';
-import StatType from '../Character/Stats/StatType';
 import { getRandomEncounter } from '../encounters';
+import { weapons } from '../Equipment/Weapon';
 
 const testChar = new Character({
     name: 'Test',
@@ -21,6 +22,14 @@ const left = battle.left[0];
 const right = battle.right[0];
 
 function displayCharacterStats(char: Character) {
+    const weapon = char.equipment.mainHand ?? weapons.unarmed0;
+    const damageRange = char.calcDamageRange({
+        attackType: weapon.attackType ?? AttackType.MeleeWeapon,
+        damageRange: weapon.damageRange,
+        spellPowerRatio: weapon.spellPowerRatio,
+        isOffHand: false
+    });
+    
     console.log(`${char.name}
 Lvl. ${char.level}
 Health: ${char.currentHealth}
@@ -28,6 +37,7 @@ Hit Chance: ${char.stats.hitChance}
 Dodge Chance: ${char.stats.dodge}
 Crit Chance: ${char.stats.critChance}
 Crit Damage: ${char.stats.critDamage}
+Damage Range: ${damageRange.min}-${damageRange.max}
 `);
 }
 
