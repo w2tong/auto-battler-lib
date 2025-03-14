bd77119892782b72610da9dca16c870c80a294a8
Wayne Tong <dkotrzarashi@gmail.com>
2025-03-14 16:44:32 -0400

    fix dice formatting

--- src/dice.ts
+++ src/dice.ts
@@ -1,20 +1,20 @@
-type Dice = {num: number, sides: number}
+type Dice = { num: number, sides: number }
 
-type DiceRoll = '1d2'|'1d3'|'1d4'|'1d6'|'1d8'|'1d10'|'1d12'|'1d20'|'1d100'
-| '2d6'
-const dice: {[key in DiceRoll]: Dice} = {
+type DiceRoll = '1d2' | '1d3' | '1d4' | '1d6' | '1d8' | '1d10' | '1d12' | '1d20' | '1d100'
+    | '2d6'
+const dice: { [key in DiceRoll]: Dice } = {
     // 1 die
-    '1d2': {num: 1, sides: 2},
-    '1d3': {num: 1, sides: 3},
-    '1d4': {num: 1, sides: 4},
-    '1d6': {num: 1, sides: 6},
-    '1d8': {num: 1, sides: 8},
-    '1d10': {num: 1, sides: 10},
-    '1d12': {num: 1, sides: 12},
-    '1d20': {num: 1, sides: 20},
-    '1d100': {num: 1, sides: 100},
+    '1d2': { num: 1, sides: 2 },
+    '1d3': { num: 1, sides: 3 },
+    '1d4': { num: 1, sides: 4 },
+    '1d6': { num: 1, sides: 6 },
+    '1d8': { num: 1, sides: 8 },
+    '1d10': { num: 1, sides: 10 },
+    '1d12': { num: 1, sides: 12 },
+    '1d20': { num: 1, sides: 20 },
+    '1d100': { num: 1, sides: 100 },
     // 2 die
-    '2d6': {num: 2, sides: 6},
+    '2d6': { num: 2, sides: 6 },
 } as const;
 
 function rollDice(dice: Dice): number {
