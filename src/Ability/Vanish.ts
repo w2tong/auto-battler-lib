a8e9c06ee18544d6cd6a27aba16a2be719c6834a
Wayne Tong <dkotrzarashi@gmail.com>
2025-03-20 19:30:56 -0400

    fix Vanish to still be usable when not in battle

--- src/Ability/Vanish.ts
+++ src/Ability/Vanish.ts
@@ -8,9 +8,8 @@ const Vanish: Ability = {
     name: NAME,
     description: `Gain ${DEX_RATIO * 100}% DEX (rounded down, min 1) ${BuffId.Invisible} stacks, causing your next attack to be a sneak attack.`,
     func: (char) => {
-        if (!char.battle) return;
         char.useAbilityMana();
-        char.battle.ref.log.add(`${char.name} used ${NAME}.`);
+        if (char.battle) char.battle.ref.log.add(`${char.name} used ${NAME}.`);
         char.statusEffectManager.addBuff(BuffId.Invisible, char, Math.max(Math.floor(char.attributes.dexterity * DEX_RATIO), 1));
     }
 };
