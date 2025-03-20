7818c9ff3187de5b2100daaae442bd02f6b13d01
Wayne Tong <dkotrzarashi@gmail.com>
2025-03-20 19:10:28 -0400

    add Firebolt tests

---
+++ src/Ability/Firebolt.test.ts
@@ -0,0 +1,80 @@
+import Battle from '../Battle';
+import Character from '../Character/Character';
+import StatType from '../Character/Stats/StatType';
+import DebuffId from '../StatusEffect/DebuffId';
+import { getCharBattleId } from '../util';
+import Firebolt from './Firebolt';
+
+let char: Character;
+let target: Character;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let battle: Battle;
+
+beforeEach(() => {
+    char = new Character({
+        name: 'Test',
+        level: 10,
+        attributes: {},
+        statTemplate: {
+            [StatType.MaxHealth]: { base: 100 },
+            [StatType.SpellPower]: { base: 100 },
+            [StatType.StartingMana]: { base: 100 },
+            [StatType.ManaCost]: { base: 50 }
+        },
+        equipment: {},
+        ability: Firebolt
+    });
+
+    target = new Character({
+        name: 'Target',
+        level: 1,
+        attributes: {},
+        statTemplate: {
+            [StatType.MaxHealth]: { base: 100 }
+        },
+        equipment: {}
+    });
+
+    char.target = target;
+    battle = new Battle([char], [target]);
+});
+
+let mathRandomSpy: jest.SpyInstance;
+beforeEach(() => {
+    mathRandomSpy = jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+});
+afterEach(() => {
+    mathRandomSpy.mockRestore();
+});
+
+let hitRollSpy: jest.SpyInstance;
+beforeEach(() => {
+    hitRollSpy = jest.spyOn(Character.prototype, 'hitRoll');
+});
+afterEach(() => {
+    hitRollSpy.mockRestore();
+});
+
+test('Firebolt Hit', () => {
+    hitRollSpy.mockReturnValue(true);
+    Firebolt.func(char);
+    expect(target.currentHealth).toBe(62);
+    expect(char.currentMana).toBe(50);
+    expect(target.statusEffectManager.debuffs[DebuffId.Burn]?.instances).toHaveProperty(getCharBattleId(char), { source: char, stacks: 2 });
+});
+
+test('Firebolt Miss', () => {
+    hitRollSpy.mockReturnValue(false);
+    Firebolt.func(char);
+    expect(target.currentHealth).toBe(100);
+    expect(char.currentMana).toBe(50);
+    expect(target.statusEffectManager.debuffs[DebuffId.Burn]).toBeUndefined();
+});
+
+test('Firebolt No Target', () => {
+    char.target = null;
+    Firebolt.func(char);
+    expect(target.currentHealth).toBe(100);
+    expect(char.currentMana).toBe(100);
+    expect(target.statusEffectManager.debuffs[DebuffId.Burn]).toBeUndefined();
+});
