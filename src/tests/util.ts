5618845418308c44a17638d9f8a42ae1f7b7b59b
Wayne Tong <dkotrzarashi@gmail.com>
2025-04-11 21:28:36 -0400

    add options param to createTestCharacter

--- src/tests/util.ts
+++ src/tests/util.ts
@@ -6,16 +6,20 @@ import { StatTemplate } from '../Character/Stats/StatTemplate';
 import DamageType from '../DamageType';
 import { EquipmentImport } from '../Equipment/Equipment';
 import { ItemType } from '../Equipment/Item';
+import { Potion } from '../Equipment/Potion';
 import { Weapon, WeaponType } from '../Equipment/Weapon/Weapon';
 
-function createTestCharacter({ level = 1, attributes = {}, statTemplate = {}, equipment = { mainHand: test1HWeapon }, ability }: { level?: number, attributes?: BaseAttributes, statTemplate?: StatTemplate, equipment?: EquipmentImport, ability?: Ability; }): Character {
+function createTestCharacter({ level = 1, attributes = {}, statTemplate = {}, equipment = { mainHand: test1HWeapon }, ability, options }: {
+    level?: number, attributes?: BaseAttributes, statTemplate?: StatTemplate, equipment?: EquipmentImport, ability?: Ability, options?: { currHealthPc?: number, currManaPc?: number; };
+}): Character {
     return new Character({
         name: '',
         level,
         attributes,
         statTemplate,
         equipment,
-        ability
+        ability,
+        options
     });
 }
 
@@ -43,4 +47,15 @@ const test2HWeapon: Weapon = {
     damageRange: { min: 0, max: 0, bonus: 0 },
 };
 
-export { createTestCharacter, test1HWeapon, test2HWeapon };
+const testPotion: Potion = {
+    id: '',
+    itemType: ItemType.Potion,
+    name: '',
+    tier: 0,
+    img: '',
+    dice: { num: 2, sides: 4 },
+    bonus: 2,
+    charges: 1
+};
+
+export { createTestCharacter, test1HWeapon, test2HWeapon, testPotion };
