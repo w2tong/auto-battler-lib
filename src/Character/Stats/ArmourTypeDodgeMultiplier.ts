b3fe6595126c6eea1043e2a79300eadab64f8f05
Wayne Tong <dkotrzarashi@gmail.com>
2025-05-31 22:36:44 -0400

    Reduce dodge multiplier for medium and heavy armour

--- src/Character/Stats/ArmourTypeDodgeMultiplier.ts
+++ src/Character/Stats/ArmourTypeDodgeMultiplier.ts
@@ -3,8 +3,8 @@ import { ArmourType } from '../../Equipment/Armour';
 const ArmourTypeDodgeMultiplier: { [type in ArmourType]: number } = {
     [ArmourType.Unarmoured]: 1,
     [ArmourType.Light]: 0.8,
-    [ArmourType.Medium]: 0.5,
-    [ArmourType.Heavy]: 0.25,
+    [ArmourType.Medium]: 0.4,
+    [ArmourType.Heavy]: 0.2,
 };
 
 export default ArmourTypeDodgeMultiplier;
