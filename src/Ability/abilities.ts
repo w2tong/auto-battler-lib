1f15742ebaa296951af20957f5edc9ae6aefc5ce
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-06 22:34:02 -0400

    update abilities object with new abilities

--- src/Ability/abilities.ts
+++ src/Ability/abilities.ts
@@ -5,16 +5,31 @@ import Bless from './Bless';
 import DoubleStrike from './DoubleStrike';
 import FerociousBite from './FerociousBite';
 import Firebolt from './Firebolt';
+import SerpentSting from './SerpentSting';
+import ShieldWall from './ShieldWall';
 import Vanish from './Vanish';
 import WoundingShot from './WoundingShot';
 
 const abilities: Record<AbilityId, Ability> = {
-    [AbilityId.Bless]: Bless,
+    // Fighter
     [AbilityId.DoubleStrike]: DoubleStrike,
-    [AbilityId.FerociousBite]: FerociousBite,
-    [AbilityId.Firebolt]: Firebolt,
-    [AbilityId.Vanish]: Vanish,
+    [AbilityId.ShieldWall]: ShieldWall,
+
+    // Priest
+    [AbilityId.Bless]: Bless,
+
+    // Ranger
+    [AbilityId.SerpentSting]: SerpentSting,
     [AbilityId.WoundingShot]: WoundingShot,
+
+    // Rogue
+    [AbilityId.Vanish]: Vanish,
+
+    // Wizard
+    [AbilityId.Firebolt]: Firebolt,
+
+    // Other
+    [AbilityId.FerociousBite]: FerociousBite,
 } as const;
 
 export default abilities;
