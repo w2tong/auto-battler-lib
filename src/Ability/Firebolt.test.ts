be35c2cfdd379b29ce1fe59a6e433747a32aa907
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-09 17:20:40 -0400

    Update Firebolt description and add tests

--- src/Ability/Firebolt.test.ts
+++ src/Ability/Firebolt.test.ts
@@ -6,6 +6,9 @@ import { createTestCharacter } from '../tests/util';
 import { getCharBattleId } from '../util';
 import Firebolt from './Firebolt';
 
+describe('Firebolt Damage and Burning Debuff', () => {
+
+});
 let char: Character;
 let target: Character;
 
@@ -30,42 +33,58 @@ beforeEach(() => {
     new Battle([char], [target]);
 });
 
-let mathRandomSpy: jest.SpyInstance;
-beforeEach(() => {
-    mathRandomSpy = jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
-});
-afterEach(() => {
-    mathRandomSpy.mockRestore();
+describe('Firebolt Damage and Debuff', () => {
+    let mathRandomSpy: jest.SpyInstance;
+    beforeEach(() => {
+        mathRandomSpy = jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+    });
+    afterEach(() => {
+        mathRandomSpy.mockRestore();
+    });
+
+    let hitRollSpy: jest.SpyInstance;
+    beforeEach(() => {
+        hitRollSpy = jest.spyOn(Character.prototype, 'hitRoll');
+    });
+    afterEach(() => {
+        hitRollSpy.mockRestore();
+    });
+
+    test('Firebolt Hit', () => {
+        hitRollSpy.mockReturnValue(true);
+        Firebolt.func(char);
+        expect(target.currentHealth).toBe(62);
+        expect(char.currentMana).toBe(50);
+        expect(target.statusEffectManager.debuffs[DebuffId.Burning]![getCharBattleId(char)].stacks).toBe(2);
+    });
+
+    test('Firebolt Miss', () => {
+        hitRollSpy.mockReturnValue(false);
+        Firebolt.func(char);
+        expect(target.currentHealth).toBe(100);
+        expect(char.currentMana).toBe(50);
+        expect(target.statusEffectManager.debuffs[DebuffId.Burning]).toBeUndefined();
+    });
+
+    test('Firebolt No Target', () => {
+        char.target = null;
+        Firebolt.func(char);
+        expect(target.currentHealth).toBe(100);
+        expect(char.currentMana).toBe(100);
+        expect(target.statusEffectManager.debuffs[DebuffId.Burning]).toBeUndefined();
+    });
 });
 
-let hitRollSpy: jest.SpyInstance;
-beforeEach(() => {
-    hitRollSpy = jest.spyOn(Character.prototype, 'hitRoll');
-});
-afterEach(() => {
-    hitRollSpy.mockRestore();
-});
 
-test('Firebolt Hit', () => {
-    hitRollSpy.mockReturnValue(true);
-    Firebolt.func(char);
-    expect(target.currentHealth).toBe(62);
-    expect(char.currentMana).toBe(50);
-    expect(target.statusEffectManager.debuffs[DebuffId.Burning]![getCharBattleId(char)].stacks).toBe(2);
-});
-
-test('Firebolt Miss', () => {
-    hitRollSpy.mockReturnValue(false);
-    Firebolt.func(char);
-    expect(target.currentHealth).toBe(100);
-    expect(char.currentMana).toBe(50);
-    expect(target.statusEffectManager.debuffs[DebuffId.Burning]).toBeUndefined();
-});
-
-test('Firebolt No Target', () => {
-    char.target = null;
-    Firebolt.func(char);
-    expect(target.currentHealth).toBe(100);
-    expect(char.currentMana).toBe(100);
-    expect(target.statusEffectManager.debuffs[DebuffId.Burning]).toBeUndefined();
+describe('Firebolt Description', () => {
+    test('No Character', () => {
+        expect(Firebolt.description()).toBe(
+            'Deals damage to your target and apply 2 Burning, dealing damage each turn.'
+        );
+    });
+    test('Wth Character', () => {
+        expect(Firebolt.description(char)).toBe(
+            'Deals 36 - 40 damage to your target and apply 2 Burning, dealing 21 damage each turn.'
+        );
+    });
 });
