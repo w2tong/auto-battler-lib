9f29d92cc56d3e163b0af80f48c63ef9154fea9b
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-11 20:06:12 -0400

    update simulation to log more concisely

--- src/tests/simulation.ts
+++ src/tests/simulation.ts
@@ -2,6 +2,7 @@ import Battle, { Side, TurnRes } from '../Battle/Battle';
 
 import abilities from '../Ability/abilities';
 import AbilityId from '../Ability/AbilityId';
+import AttributeType from '../Character/Attributes/AttributeType';
 import BaseAttributes from '../Character/Attributes/BaseAttributes';
 import Character from '../Character/Character';
 import ClassName from '../Character/Classes/ClassName';
@@ -17,11 +18,17 @@ import { potions } from '../Equipment/Potion';
 import { rings } from '../Equipment/Ring';
 import { shields } from '../Equipment/Shield';
 import { weapons } from '../Equipment/Weapon/weapons';
+import { waists } from '../Equipment/Waist';
 
 import NPC from '../npc/NPC';
 import { createNpcChars } from '../encounters';
+
+import BanditFighter from '../npc/bandit/BanditFighter';
+import BanditPriest from '../npc/bandit/BanditPriest';
+
+import OrcFighter from '../npc/OrcFighter';
 import OgreFighter from '../npc/OgreFighter';
-import AttributeType from '../Character/Attributes/AttributeType';
+
 
 type CharConstructorArgs = {
     name: string,
@@ -33,48 +40,258 @@ type CharConstructorArgs = {
     ability: AbilityId,
     petId?: PetId;
 };
+type Player = { name: string, level: Record<number, CharConstructorArgs>; };
 
 type SimulationResults = { leftWins: number, rightWins: number, ties: number; };
 type SimNPCs = { chars: NPC[], level: number; };
 
-const fighter5: CharConstructorArgs = {
+const fighter: Player = {
     name: 'Fighter',
-    level: 5,
-    className: ClassName.Fighter,
+    level: {
+        5: {
+            name: 'Fighter',
+            level: 5,
+            className: ClassName.Fighter,
 
-    // Free points
-    // 10 starting points
-    // 12 points from levels
-    // 10 points from 5 INT and WIS
-    // 32 Total
+            // Free points
+            // 10 starting points
+            // 12 points from levels
+            // 10 points from 5 INT and WIS
+            // 32 Total
 
-    // Points spent
-    // +10 STR
-    // +10 PER
-    // +10 CON
-    // +2
-    // 32 Total
-    attributes: {
-        [AttributeType.Strength]: 25,
-        [AttributeType.Dexterity]: 12,
-        [AttributeType.Perception]: 20,
-        [AttributeType.Constitution]: 25,
-        [AttributeType.Intelligence]: 5,
-        [AttributeType.Wisdom]: 5
-    },
-    statTemplate: {},
-    equipment: {
-        [EquipSlot.MainHand]: weapons.longsword1,
-        [EquipSlot.OffHand]: shields.buckler1,
-        [EquipSlot.Armour]: armour.plateArmour1,
-        [EquipSlot.Head]: heads.helmet0,
-        [EquipSlot.Hands]: hands.ohGloves0,
-        [EquipSlot.Ring1]: rings.accRing0,
-        [EquipSlot.Ring2]: rings.accRing0,
-        [EquipSlot.Potion]: potions.healingPotion0,
-        [EquipSlot.Neck]: necks.strNeck0
-    },
-    ability: AbilityId.DoubleStrike,
+            // Points spent
+            // +10 STR
+            // + 2 DEX
+            // +10 PER
+            // +10 CON
+
+            // 32 Total
+            attributes: {
+                [AttributeType.Strength]: 20,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 20,
+                [AttributeType.Constitution]: 20,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword1,
+                [EquipSlot.OffHand]: shields.buckler1,
+                [EquipSlot.Armour]: armour.plateArmour0,
+                [EquipSlot.Head]: heads.helmet0,
+                [EquipSlot.Hands]: hands.ohGloves0,
+                [EquipSlot.Ring1]: rings.accRing0,
+                [EquipSlot.Ring2]: rings.dmgRing0,
+                [EquipSlot.Potion]: potions.healingPotion0,
+                [EquipSlot.Neck]: necks.strNeck0
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        8: {
+            name: 'Fighter',
+            level: 8,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +19 STR
+            // + 2 DEX
+            // +10 PER
+            // +10 CON
+            // 41 Total
+            attributes: {
+                [AttributeType.Strength]: 29,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 20,
+                [AttributeType.Constitution]: 20,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword1,
+                [EquipSlot.OffHand]: shields.buckler1,
+                [EquipSlot.Armour]: armour.plateArmour1,
+                [EquipSlot.Head]: heads.helmet0,
+                [EquipSlot.Hands]: hands.ohGloves0,
+                [EquipSlot.Ring1]: rings.accRing0,
+                [EquipSlot.Ring2]: rings.dmgRing0,
+                [EquipSlot.Potion]: potions.healingPotion0,
+                [EquipSlot.Neck]: necks.strNeck0
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        9: {
+            name: 'Fighter',
+            level: 9,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +20 STR
+            // + 2 DEX
+            // +12 PER
+            // +10 CON
+            // 44 Total
+            attributes: {
+                [AttributeType.Strength]: 30,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 22,
+                [AttributeType.Constitution]: 20,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword2,
+                [EquipSlot.OffHand]: shields.buckler2,
+                [EquipSlot.Armour]: armour.plateArmour2,
+                [EquipSlot.Head]: heads.helmet0,
+                [EquipSlot.Hands]: hands.ohGloves0,
+                [EquipSlot.Ring1]: rings.accRing0,
+                [EquipSlot.Ring2]: rings.accRing0,
+                [EquipSlot.Potion]: potions.healingPotion1,
+                [EquipSlot.Neck]: necks.strNeck0,
+                [EquipSlot.Waist]: waists.healBelt0
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        12: {
+            name: 'Fighter',
+            level: 12,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +29 STR
+            // + 2 DEX
+            // +12 PER
+            // +10 CON
+            // 53 Total
+            attributes: {
+                [AttributeType.Strength]: 39,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 22,
+                [AttributeType.Constitution]: 22,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword2,
+                [EquipSlot.OffHand]: shields.buckler2,
+                [EquipSlot.Armour]: armour.plateArmour2,
+                [EquipSlot.Head]: heads.helmet0,
+                [EquipSlot.Hands]: hands.ohGloves0,
+                [EquipSlot.Ring1]: rings.accRing0,
+                [EquipSlot.Ring2]: rings.dmgRing0,
+                [EquipSlot.Potion]: potions.healingPotion1,
+                [EquipSlot.Neck]: necks.strNeck0,
+                [EquipSlot.Waist]: waists.healBelt0
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        13: {
+            name: 'Fighter',
+            level: 13,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +30 STR
+            // + 2 DEX
+            // +12 PER
+            // +12 CON
+            // 56 Total
+            attributes: {
+                [AttributeType.Strength]: 40,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 22,
+                [AttributeType.Constitution]: 22,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword3,
+                [EquipSlot.OffHand]: shields.buckler3,
+                [EquipSlot.Armour]: armour.plateArmour3,
+                [EquipSlot.Head]: heads.helmet1,
+                [EquipSlot.Hands]: hands.ohGloves1,
+                [EquipSlot.Ring1]: rings.accRing1,
+                [EquipSlot.Ring2]: rings.dmgRing1,
+                [EquipSlot.Potion]: potions.healingPotion2,
+                [EquipSlot.Neck]: necks.strNeck1,
+                [EquipSlot.Waist]: waists.healBelt1
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        17: {
+            name: 'Fighter',
+            level: 17,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +40 STR
+            // + 2 DEX
+            // +12 PER
+            // +14 CON
+            // 68 Total
+            attributes: {
+                [AttributeType.Strength]: 50,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 22,
+                [AttributeType.Constitution]: 24,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword4,
+                [EquipSlot.OffHand]: shields.buckler4,
+                [EquipSlot.Armour]: armour.plateArmour4,
+                [EquipSlot.Head]: heads.helmet1,
+                [EquipSlot.Hands]: hands.ohGloves1,
+                [EquipSlot.Ring1]: rings.accRing1,
+                [EquipSlot.Ring2]: rings.dmgRing1,
+                [EquipSlot.Potion]: potions.healingPotion3,
+                [EquipSlot.Neck]: necks.strNeck1,
+                [EquipSlot.Waist]: waists.healBelt2
+            },
+            ability: AbilityId.DoubleStrike,
+        },
+        20: {
+            name: 'Fighter',
+            level: 20,
+            className: ClassName.Fighter,
+
+            // Points spent
+            // +40 STR
+            // + 2 DEX
+            // +15 PER
+            // +14 CON
+            // 71 Total
+            attributes: {
+                [AttributeType.Strength]: 50,
+                [AttributeType.Dexterity]: 12,
+                [AttributeType.Perception]: 25,
+                [AttributeType.Constitution]: 24,
+                [AttributeType.Intelligence]: 5,
+                [AttributeType.Wisdom]: 5
+            },
+            statTemplate: {},
+            equipment: {
+                [EquipSlot.MainHand]: weapons.longsword5,
+                [EquipSlot.OffHand]: shields.buckler5,
+                [EquipSlot.Armour]: armour.plateArmour5,
+                [EquipSlot.Head]: heads.helmet2,
+                [EquipSlot.Hands]: hands.ohGloves2,
+                [EquipSlot.Ring1]: rings.accRing2,
+                [EquipSlot.Ring2]: rings.dmgRing2,
+                [EquipSlot.Potion]: potions.healingPotion4,
+                [EquipSlot.Neck]: necks.strNeck2,
+                [EquipSlot.Waist]: waists.healBelt3
+            },
+            ability: AbilityId.DoubleStrike,
+        }
+    }
 };
 
 function simulateBattles(player: CharConstructorArgs, npcs: SimNPCs, battles: number): SimulationResults {
@@ -117,22 +334,25 @@ function simulateBattles(player: CharConstructorArgs, npcs: SimNPCs, battles: nu
     return { leftWins, rightWins, ties };
 }
 
-function printTitle(player: CharConstructorArgs, npcs: SimNPCs, battles: number) {
-    console.log(
-        `Lvl. ${player.level} ${player.name} vs Lvl. ${npcs.level} ${npcs.chars.map(npc => npc.name).join(', ')} (${battles})`
-    );
-}
-
-function printRes(res: SimulationResults, total: number) {
-    console.log(`Player: ${res.leftWins / total * 100}%`);
-    console.log(`NPC: ${res.rightWins / total * 100}%`);
-    console.log(`Tie: ${res.ties / total * 100}%`);
-}
-
 function simAndPrint(player: CharConstructorArgs, npcs: SimNPCs, battles: number) {
-    printTitle(player, npcs, battles);
     const res = simulateBattles(player, npcs, battles);
-    printRes(res, battles);
+    const wins = `W: ${(res.leftWins / battles * 100).toFixed(1)}%`;
+    // const losses = `L: ${res.rightWins / total * 100}%`;
+    // const ties = `T: ${res.ties / total * 100}%`;
+    console.log(`${player.level}:\t${wins}`);
 }
 
-simAndPrint(fighter5, { chars: [OgreFighter], level: 5 }, 1000);
+// Fighter vs. Orc Fighter
+function fullSim(player: Player, npcs: NPC[], battles: number) {
+    console.log(`${player.name} vs ${npcs.map(npc => npc.name).join(', ')} (${battles} battles)`);
+    for (const char of Object.values(player.level)) {
+        simAndPrint(char, { chars: npcs, level: char.level }, battles);
+    }
+}
+
+// fullSim(fighter, [BanditFighter], 1000);
+// fullSim(fighter, [BanditPriest], 1000);
+// fullSim(fighter, [OrcFighter], 1000);
+fullSim(fighter, [OgreFighter], 1000);
+// Fighter vs. Ogre Fighter
+// simAndPrint(fighter5, { chars: [OgreFighter], level: 5 }, 1000);
