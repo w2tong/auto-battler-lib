7fca1636c70fea1391ea8333b4bdd3462431877c
Wayne Tong <dkotrzarashi@gmail.com>
2025-06-18 16:12:52 -0400

    update accuracy tests to use DEFAULT_ACCURACY

--- src/Character/Stats/StatDescriptions.test.ts
+++ src/Character/Stats/StatDescriptions.test.ts
@@ -2,5 +2,5 @@ import StatDescriptions from './StatDescriptions';
 import StatType from './StatType';
 
 test('Dodge Description', () => {
-    expect(StatDescriptions[StatType.Dodge]()).toBe('Affects the chance of avoiding an incoming attack. Dodge is affected by the type of armour the character is wearing.\n\nThe character\'s dodge is multipled by value of the armour worn.\nUnarmoured: 100%\nLight Armour: 70%\nMedium Armour: 40%\nHeavy Armour: 0%');
+    expect(StatDescriptions[StatType.Dodge]()).toBe('Affects the chance of avoiding an incoming attack. Dodge is affected by the type of armour the character is wearing.\n\nThe character\'s dodge is multipled by value of the armour worn.\nUnarmoured: 100%\nLight Armour: 70%\nMedium Armour: 40%\nHeavy Armour: 10%');
 });
